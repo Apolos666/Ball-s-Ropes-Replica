47c660eded0a818e037fd1e5451347bddebfeb63
Apolos666 <quangprox121@gmail.com>
2023-10-30 20:43:25 +0700

    backup

--- Assets/_Scripts/Core/Rope/ObjectOverlapPrevent.cs
+++ Assets/_Scripts/Core/Rope/ObjectOverlapPrevent.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Apolos.Core;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -10,8 +11,15 @@ public class ObjectOverlapPrevent : MonoBehaviour
     private Vector3 _snapDir;
     [SerializeField] private float _configNumber = 0.15f;
     [SerializeField] private Draggable _draggable;
-    private bool _isOnTriggerStay;
     private bool _isGOMoving;
+    private bool _firstEnterTrigger = true;
+
+    private List<Collider> _triggerColliders;
+
+    private void Awake()
+    {
+        _triggerColliders = new List<Collider>();
+    }
 
     private void OnDrawGizmos()
     {
@@ -20,33 +28,55 @@ public class ObjectOverlapPrevent : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
+        if (!_triggerColliders.Contains(other))
+        {
+            _triggerColliders.Add(other);
+        }
+        
         if (_isGOMoving)
         {
-            if (Vector3.Distance(other.ClosestPoint(_ropePointTransform.position), transform.position) == 0)
-            {
-                return;
-            }
+            if (_triggerColliders.Count > 0 && !_firstEnterTrigger) return;
+            
+            if (IsInSideAnotherObject(other)) return;
+            
             _closestPoint =  other.ClosestPoint(_ropePointTransform.position);
             _snapDir = (_closestPoint - _ropePointTransform.position).normalized;
+            _firstEnterTrigger = false;
         }
+    }
 
-        _isOnTriggerStay = true;
+    private bool IsInSideAnotherObject(Collider other)
+    {
+        return Vector3.Distance(other.ClosestPoint(_ropePointTransform.position), transform.position) == 0;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        _isOnTriggerStay = false;
+        print(other.gameObject.name);
+        
+        if (_triggerColliders.Contains(other))
+        {
+            _triggerColliders.Remove(other);
+        }
+        _firstEnterTrigger = true;
+    }
+
+    private void Update()
+    {
+        if (_isGOMoving)
+        {
+            print(_triggerColliders.Count);
+        }
     }
 
     public void OnPointerUp()
     {
-        if (_isGOMoving && _isOnTriggerStay)
+        if (_isGOMoving && _triggerColliders.Count > 0)
         {
             _draggable.StopMoving();
             _ropePointTransform.position = _closestPoint;
             _ropePointTransform.position -= _snapDir * _configNumber;
         }
-
         _isGOMoving = false;    
     }
 
