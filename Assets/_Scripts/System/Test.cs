065aa0e8205d8752067b807fb112a5447bcf2fc9
Apolos666 <quangprox121@gmail.com>
2023-11-13 08:27:42 +0700

    Revert "backup before install fast script reload"
    This reverts commit 1ab91f51ef25924c8985991e6dca55d60757a4a2.

--- Assets/_Scripts/System/Test.cs
+++ Assets/_Scripts/System/Test.cs
@@ -2,23 +2,63 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Apolos.Core;
-using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.Splines;
 
 public class Test : MonoBehaviour
 {
-    private SplineAnimate _splineAnimate;
+    [SerializeField] private Vector3 point2D;
+    public bool _useTriggerEnter;
+    [SerializeField] private Vector3 dir;
+    [SerializeField] private float _configNumber = 3;
+    [SerializeField] private Draggable _draggable;
+    private bool _isOnTriggerEnter;
+    private bool _isGOMoving;
 
-    private void Awake()
+    private void OnTriggerEnter(Collider other)
     {
-        _splineAnimate = GetComponent<SplineAnimate>();
-        
+        if (_isGOMoving)
+        {
+            print("On Triggger Enter");
+            point2D =  other.ClosestPoint(transform.position);
+            dir = (point2D - transform.position).normalized;
+        }
+
+        _isOnTriggerEnter = true;
     }
 
-    [Button("Change Value")]
-    private void ChangeValue()
+    private void OnTriggerExit(Collider other)
     {
-        _splineAnimate.NormalizedTime = 0.5f;
+        if (_isGOMoving)
+        {
+            print("On Trigger Exit");
+            point2D = Vector3.zero;
+        }
+
+        _isOnTriggerEnter = false;
+    }
+
+    public void OnPointerUp()
+    {
+        if (_isGOMoving && _isOnTriggerEnter)
+        {
+            _draggable.StopMoving();
+            transform.position = point2D;
+            transform.position -= dir * _configNumber;
+        }
+
+        _isGOMoving = false;    
+    }
+
+    public void OnDrag()
+    {
+        _isGOMoving = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_useTriggerEnter)
+        {
+            Gizmos.DrawWireSphere(point2D, 0.1f);
+        }
     }
 }
